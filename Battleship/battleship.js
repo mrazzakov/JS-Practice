4fe06bdc98637a6757cd7b9089b569423d56b1e4
unknown <mrazzakov@gmail.com>
2019-02-11 06:48:41 -0600

    change folder structure; add battleship game

---
+++ Battleship/battleship.js
@@ -0,0 +1,17 @@
+var location1 = 3;
+var location2 = 4;
+var location3 = 5;
+
+var guess;
+var hits = 0;
+var guesses = 0;
+
+var isSunk = false;
+
+while(!isSunk) {
+    guess = prompt("Ready, Aim, Fire! (enter a number from 0-6):");
+
+    //if(guess == location1 || guess == location2 || guess == location3) {
+
+  //  }
+}
