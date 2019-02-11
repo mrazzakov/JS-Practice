a5c120442e822ef2447047fb7abb50058577ff8b
unknown <mrazzakov@gmail.com>
2019-02-11 07:40:44 -0600

    init battleship 2d

--- Battleship/battleship.js
+++ Battleship/battleship.js
@@ -8,10 +8,23 @@ var guesses = 0;
 
 var isSunk = false;
 
+//Main logic
 while(!isSunk) {
-    guess = prompt("Ready, Aim, Fire! (enter a number from 0-6):");
-
-    //if(guess == location1 || guess == location2 || guess == location3) {
-
-  //  }
+  guess = prompt("Ready, Aim, Fire! (enter a number from 0-6):");
+  if(guess < 0 || guess > 6) {
+    alert("Please Enter A Valid Number!");
+  } else {
+    guesses++;
+  }
+  if(guess == location1 || guess == location2 || guess == location3) {
+    hits++;
+    alert("Hit!")
+    if(hits > 2) {
+        alert("You Sank My Battleship!");
+        isSunk = true;
+    }
+  }
+  else
+    alert("Miss!")
 }
+alert("Number of Guesses: " + guesses);
