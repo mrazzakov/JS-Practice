348c8b0f0e19c4f44fa38a4dba613f4912e02d89
unknown <mrazzakov@gmail.com>
2019-02-15 23:58:02 -0600

    outputs winner

--- Battleship/battleship.js
+++ Battleship/battleship.js
@@ -2,28 +2,9 @@
 var hit = ['A7', 'B7', 'C7', 'D2', 'D3', 'D4', 'D5', 'A3', 'B3'];
 var hitSuccess = [false];
 var selected;
-//Main logic
-// while(!isSunk) {
-//   guess = prompt("Ready, Aim, Fire! (enter a number from 0-6):");
-//   if(guess < 0 || guess > 6) {
-//     alert("Please Enter A Valid Number!");
-//   } else {
-//     guesses++;
-//   }
-//   if(guess == location1 || guess == location2 || guess == location3) {
-//     selectButtons++;
-//     alert("selectButton!")
-//     if(selectButtons > 2) {
-//         alert("You Sank My Battleship!");
-//         isSunk = true;
-//     }
-//   }
-//   else
-//     alert("Miss!")
-// }
-// alert("Number of Guesses: " + guesses);
+
 function checkSuccess(hitsuccess) {
-  return hitSuccess === true;
+  return hitsuccess == true;
 }
 
 function selectButton(id) {
@@ -39,7 +20,7 @@ function selectButton(id) {
 
   console.log(hit.length);
   console.log(hitSuccess.every(checkSuccess));
-  if(hitSuccess.every(checkSuccess)) {
+  if(hitSuccess.every(checkSuccess) && hitSuccess.length === hit.length) {
     console.log("you win");
   }
 }
